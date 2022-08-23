79933ddcb21abb99bd45e0692799c07bd9f45f8e
patonz <patonz91@gmail.com>
2022-08-23 17:56:47 +0200

    update for build

--- src/build.sh
+++ src/build.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
 export TAG=$1
 echo building with tag $TAG
-docker compose -f build.yml up
-
-
-function push {
-    docker push patonz/uhura:core_${TAG}
-    
-    docker push patonz/uhura:gateway_${TAG}
-    
-    docker push patonz/uhura:discovery_${TAG}
-    
-    docker push patonz/uhura:adapter_nats_${TAG}
-}
-
-while true; do
-    read -p "Do you wish to push on dockerhub? " yn
-    case $yn in
-        [Yy]* ) push; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
+docker buildx bake -f build.yml --push --set *.platform=linux/amd64,linux/arm64,linux/arm/v7
