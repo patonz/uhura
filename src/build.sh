17b836e10d4abd1d6929b8d74e88095d6324dbc2
patonz <leonardo.montecchiari@tii.ae>
2022-09-30 20:27:52 +0400

    fix on discovery

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export TAG=$1
 echo building with tag $TAG
-docker buildx bake -f build.yml 
-#--push
+docker buildx bake -f build.yml --push
 # --set *.platform=linux/amd64,linux/arm64,linux/arm/v7
